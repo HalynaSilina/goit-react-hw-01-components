7be0dc315911006f47444713994d7ee34fa81866
HalynaSilina <halynasilina@gmail.com>
2023-04-27 23:45:19 +0300

    add TransactionsHistory

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import userData from 'user.json';
 import statisticData from 'data.json';
 import friendsData from 'friends.json';
+import transactionsData from 'transactions.json';
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendsList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
@@ -22,6 +24,7 @@ export const App = () => {
       <Profile user={userData} />
       <Statistics data={statisticData} title="Upload stats" />
       <FriendsList friends={friendsData}/>
+      <TransactionHistory items={transactionsData}/>
     </div>
   );
 };
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+import { TransactionEvent } from './TransactionEvent';
+
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(item => (
+          <tr key={item.id}>
+            <TransactionEvent
+              type={item.type}
+              amount={item.amount}
+              currency={item.currency}
+            />
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
