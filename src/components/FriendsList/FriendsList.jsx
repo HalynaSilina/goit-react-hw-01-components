610c3379d1c3f7d857de2796be2b6fed160d04e6
HalynaSilina <halynasilina@gmail.com>
2023-04-28 13:11:42 +0300

    update friendlist

--- src/components/FriendsList/FriendsList.jsx
+++ src/components/FriendsList/FriendsList.jsx
@@ -5,27 +5,11 @@ import css from './FriendsList.module.css';
 export const FriendsList = ({ friends }) => {
   return (
     <ul className={css.list}>
-      {friends.map(friend => (
-        <li key={friend.id} className={css.item}>
-          <FriendsListItem
-            avatar={friend.avatar}
-            name={friend.name}
-            isOnline={friend.isOnline}
-          />
-        </li>
-      ))}
+      <FriendsListItem friends={friends} />
     </ul>
   );
 };
 
-
 FriendsList.propTypes = {
-    friends: PropTypes.arrayOf(
-        PropTypes.exact({
-            avatar: PropTypes.string.isRequired,
-            name: PropTypes.string.isRequired,
-            isOnline: PropTypes.bool.isRequired,
-            id: PropTypes.number.isRequired
-        }).isRequired
-    ).isRequired,
-  };
+  friends: PropTypes.array.isRequired,
+};
--- src/components/FriendsList/FriendsListItem.jsx
+++ src/components/FriendsList/FriendsListItem.jsx
@@ -1,18 +1,26 @@
 import PropTypes from 'prop-types';
 import css from './FriendsListItem.module.css';
 
-export const FriendsListItem = ({avatar, name, isOnline}) => {
-    return (
-        <>
-        {isOnline ? <span className={`${css.status} ${css.online}`}></span> : <span className={css.status}></span>}
-        <img className={css.avatar} src={avatar} alt="User avatar"/>
-        <p className={css.name}>{name}</p>
-        </>
-    )
+export const FriendsListItem = ({ friends }) => {
+  return friends.map(friend => (
+    <li key={friend.id} className={css.item}>
+      {friend.isOnline ? (
+        <span className={`${css.status} ${css.online}`}></span>
+      ) : (
+        <span className={css.status}></span>
+      )}
+      <img className={css.avatar} src={friend.avatar} alt="User avatar" />
+      <p className={css.name}>{friend.name}</p>
+    </li>
+  ));
 };
-
 FriendsListItem.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  isOnline: PropTypes.bool.isRequired,
+  friends: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    }).isRequired
+  ).isRequired,
 };
